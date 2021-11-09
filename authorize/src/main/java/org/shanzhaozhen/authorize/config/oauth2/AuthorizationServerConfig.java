87f604da29e86e3c5f8442b449811fe1b6d76572
shanzhaozhen <204447118@qq.com>
2021-11-09 23:02:26 +0800

    更新

--- authorize/src/main/java/org/shanzhaozhen/authorize/config/oauth2/AuthorizationServerConfig.java
+++ authorize/src/main/java/org/shanzhaozhen/authorize/config/oauth2/AuthorizationServerConfig.java
@@ -1,84 +1,88 @@
 package org.shanzhaozhen.authorize.config.oauth2;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 
-import javax.sql.DataSource;
+import java.util.UUID;
 
-@Configuration
-@EnableAuthorizationServer
-@RequiredArgsConstructor
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
 
-    private final DataSource dataSource;
-
-    private final TokenStore tokenStore;
-
-    private final AuthenticationManager authenticationManager;
-
-    private final CustomUserDetailsService customUserDetailsService;
-
-
-    /**
-     * 定义令牌端点上的安全约束
-     * @param security
-     * @throws Exception
-     */
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security
-                .allowFormAuthenticationForClients()        //如果使用表单认证则需要加上
-                .checkTokenAccess("isAuthenticated()")      // oauth/check_token    公开(permitAll)/拦截(isAuthenticated)
-                .tokenKeyAccess("isAuthenticated()");       // oauth/token_key  公开(permitAll)/拦截(isAuthenticated)
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.formLogin(Customizer.withDefaults()).build();
     }
 
-    /**
-     * 定义客户端详细信息服务的配置程序。可以初始化客户详细信息，或者您可以仅引用现有库。
-     * @param clients
-     * @throws Exception
-     */
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    // @formatter:off
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("messaging-client")
+                .clientSecret("secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope("message.read")
+                .scope("message.write")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
 
-        /**
-         * client模式，没有用户的概念，直接与认证服务器交互，用配置中的客户端信息去申请accessToken，
-         * 客户端有自己的client_id,client_secret对应于用户的username,password，而客户端也拥有自己的authorities，
-         * 当采取client模式认证时，对应的权限也就是客户端自己的authorities
-         */
+        // Save registered client in db as if in-memory
+        JdbcRegisteredClientRepository registeredClientRepository = new JdbcRegisteredClientRepository(jdbcTemplate);
+        registeredClientRepository.save(registeredClient);
 
-        /**
-         * password模式，自己本身有一套用户体系，在认证时需要带上自己的用户名和密码，以及客户端的client_id,client_secret
-         * 此时，accessToken所包含的权限是用户本身的权限，而不是客户端的权限
-         */
+        return registeredClientRepository;
+    }
+    // @formatter:on
 
-
-        clients.jdbc(dataSource)
-//                .passwordEncoder(passwordEncoder)
-        ;
+    @Bean
+    public OAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate, RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
     }
 
-    /**
-     * 定义授权和令牌端点以及令牌服务
-     * 1. 密码模式下配置认证管理器 AuthenticationManager
-     * 2. 设置 AccessToken的存储介质tokenStore， 默认使用内存当做存储介质。
-     * 3. userDetailsService注入
-     * @param endpoints
-     * @throws Exception
-     */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .tokenStore(tokenStore)
-                .userDetailsService(customUserDetailsService);
+    @Bean
+    public OAuth2AuthorizationConsentService authorizationConsentService(JdbcTemplate jdbcTemplate, RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        RSAKey rsaKey = Jwks.generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    @Bean
+    public ProviderSettings providerSettings() {
+        return ProviderSettings.builder().issuer("http://auth-server:9000").build();
+    }
+
+    @Bean
+    public EmbeddedDatabase embeddedDatabase() {
+        // @formatter:off
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .addScript("org/springframework/security/oauth2/server/authorization/oauth2-authorization-schema.sql")
+                .addScript("org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql")
+                .addScript("org/springframework/security/oauth2/server/authorization/client/oauth2-registered-client-schema.sql")
+                .build();
+        // @formatter:on
     }
 
 }
