7f65e26fbf77e7c46ddbd93cb3b42adea950b08b
shanzhaozhen <204447118@qq.com>
2022-06-16 23:11:03 +0800

    Revert "更新"
    This reverts commit a11310b4

---
+++ authorize/src/main/java/org/shanzhaozhen/authorize/mapper/RegisteredClientMapper.java
@@ -0,0 +1,8 @@
+package org.shanzhaozhen.authorize.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+
+public interface RegisteredClientMapper extends BaseMapper<RegisteredClient> {
+
+}
---
+++ authorize/src/main/java/org/shanzhaozhen/authorize/pojo/entity/OAuth2RedirectUriDO.java
@@ -0,0 +1,26 @@
+package org.shanzhaozhen.authorize.pojo.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("redirect_uri")
+@Schema(description = "oauth2客户端信息DO实体")
+public class OAuth2RedirectUriDO implements Serializable {
+
+    private static final long serialVersionUID = 4512441721005677882L;
+
+    @Schema(description = "oauth2客户端id")
+    private String clientId;
+
+    @Schema(description = "客户端允许重定向的uri")
+    private String redirectUri;
+
+}
