08be692bb3da245143307ab3dcc00179e336a140
shanzhaozhen <204447118@qq.com>
2022-06-16 22:24:59 +0800

    既来之则安之，使用别人的框架就按照别人的模式走

--- authorize/src/main/java/org/shanzhaozhen/authorize/mapper/Oauth2RegisteredClientDOMapper.java
+++
@@ -1,9 +0,0 @@
-package org.shanzhaozhen.authorize.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.shanzhaozhen.authorize.pojo.entity.Oauth2RegisteredClientDO;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-
-public interface Oauth2RegisteredClientDOMapper extends BaseMapper<Oauth2RegisteredClientDO> {
-
-}
